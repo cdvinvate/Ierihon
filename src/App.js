0a4329c1279551d68b879bf20ad3aa18fbfb49d5
Дмитрий Иванцов <dmitry.iwantzov@gmail.com>
2020-08-17 23:58:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+<Header/>
+  );
+}
+
+export default App;
---
+++ src/Pages/Pointsofsales.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Pointsofsales() {
+  return (
+'points'
+  );
+}
+
+export default Pointsofsales;
